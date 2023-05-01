343b934599f1479157a5a97776095931f25cf8eb
Sakhi <78950775+Sakhi97@users.noreply.github.com>
2023-05-01 23:21:26 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import './App.css';
 import CustomerList from './components/Customerlist';
 import TrainingList from './components/Traininglist';
 import Calendar from './components/Calendar';
+import Statistics from './components/Statistics';
+
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
+
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import Drawer from '@mui/material/Drawer';
@@ -16,10 +23,14 @@ import ListItemText from '@mui/material/ListItemText';
 import { useState, useEffect } from 'react';
 import { HashRouter, Link, Route, Routes, useLocation } from 'react-router-dom';
 
-function MainContent() {
+function MainContent({ darkMode, setDarkMode }) {
   const [value, setValue] = useState('/');
   const [drawerOpen, setDrawerOpen] = useState(false);
   const location = useLocation();
+
+  const handleThemeChange = () => {
+    setDarkMode((prevDarkMode) => !prevDarkMode);
+  };
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
@@ -36,6 +47,8 @@ function MainContent() {
     setValue(location.pathname);
   }, [location]);
 
+  const shouldDisplayTabs = !['/calendar', '/statistics'].includes(location.pathname);
+
   return (
     <>
       <AppBar position="static">
@@ -51,6 +64,9 @@ function MainContent() {
           <Typography variant="h6" style={{ flexGrow: 1 }}>
             PERSONAL TRAINING APP
           </Typography>
+          <IconButton color="inherit" onClick={handleThemeChange}>
+            {darkMode ? <Brightness7Icon /> : <Brightness4Icon />}
+          </IconButton>
         </Toolbar>
       </AppBar>
       <Drawer
@@ -70,10 +86,13 @@ function MainContent() {
             <ListItem button component={Link} to="/calendar">
               <ListItemText primary="Calendar" />
             </ListItem>
+            <ListItem button component={Link} to="/statistics">
+              <ListItemText primary="Statistics" />
+            </ListItem>
           </List>
         </div>
       </Drawer>
-      {location.pathname !== '/calendar' && (
+      {shouldDisplayTabs && (
         <Tabs value={value} onChange={handleChange}>
           <Tab value="/" label="Customer List" component={Link} to="/" />
           <Tab value="/traininglist" label="Training List" component={Link} to="/traininglist" />
@@ -84,6 +103,7 @@ function MainContent() {
           <Route path="/" element={<CustomerList />} />
           <Route path="/traininglist" element={<TrainingList />} />
           <Route path="/calendar" element={<div className="calendar"><Calendar /></div>} />
+          <Route path="/statistics" element={<div className="statistics"><Statistics /></div>} />
         </Routes>
       </div>
     </>
@@ -91,13 +111,26 @@ function MainContent() {
 }
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const theme = createTheme({
+    palette: {
+      mode: darkMode ? 'dark' : 'light',
+    },
+  });
+
   return (
-    <div className="App">
-      <HashRouter basename="/">
-        <MainContent />
-      </HashRouter>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className="App">
+        <HashRouter basename="/">
+          <MainContent darkMode={darkMode} setDarkMode={setDarkMode} />
+        </HashRouter>
+      </div>
+    </ThemeProvider>
   );
 }
 
+
 export default App;
+
